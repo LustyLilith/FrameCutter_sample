b4933fb36e9645d0523e937fa99ec94652b2f339
ImNSKW <48134229+ImNSKW@users.noreply.github.com>
2019-03-02 02:53:18 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+void usage()
+{
+	printf("--Usage FrameCutter via Video--");
+}
+
+bool captureFrames(char* filename)
+{
+	bool ret = false;
+	std::string outfilename;
+
+	VideoCapture capture(filename);
+	int max_frame = (int)capture.get(CV_CAP_PROP_FRAME_COUNT);
+	
+	for (int i = 0; i < max_frame; i++)
+	{
+		Mat frameImg;
+		capture >> frameImg;
+
+		outfilename = "";
+		outfilename += filename;
+		outfilename += "_";
+		outfilename += std::to_string(i);
+		outfilename += ".jpg";
+
+		imwrite(outfilename, frameImg);
+	}
+
+	return ret;
+}
+
+
+void main(int argc, char** argv)
+{
+	if (2 < argc)
+	{
+		printf("Please check command line arguments\n");
+		return;
+	}
+		
+	captureFrames(argv[1]);
+
+	return;
+}
